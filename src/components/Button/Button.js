3e84ca5a55721cda86ad7d0c5d550ad7a4f28d60
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2023-01-23 20:16:57 +0100

    Correct the way of wiriting functions (eliminate return where possible). Add input to add new column with a specified name

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,5 @@
 import styles from './Button.module.scss';
 
-const Button = props => {
-  return (<button className={styles.button}>{props.children}</button>);
-};
+const Button = props => <button className={styles.button}>{props.children}</button>;
 
 export default Button;
